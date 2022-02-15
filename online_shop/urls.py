49aa4a9bfe70d4b183068408a62ebf497e9b7c4a
Ali.es <ali.esmailnejad96@gmail.com>
2022-02-15 18:36:37 +0330

    include rosetta urls

--- online_shop/urls.py
+++ online_shop/urls.py
@@ -17,10 +17,11 @@ from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
 
+    path('rosetta/', include('rosetta.urls')),
     prefix_default_language=False,
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
