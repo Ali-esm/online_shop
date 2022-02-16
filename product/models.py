da9de762af3a31d67e183efe9e6631b2f4e26b69
Ali.es <ali.esmailnejad96@gmail.com>
2022-02-16 22:39:13 +0330

    implement: Category Model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from core.models import BaseModel
 
-# Create your models here.
+
+class Category(BaseModel):
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    is_sub = models.BooleanField(default=False)
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+
+        if isinstance(self.parent, self.__class__):
+            self.is_sub = True
+        else:
+            self.is_sub = False
+
+    def __str__(self):
+        return f'{self.name}'
