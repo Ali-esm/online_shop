fd62fa852b22571aa3925cd3afb1e3a92722c1a6
Ali.es <ali.esmailnejad96@gmail.com>
2022-02-16 23:25:24 +0330

    install core app to settings

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Additional Installed app
+    'core',
     'product',
 ]
 
@@ -145,3 +146,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'core.User'
