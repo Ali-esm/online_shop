fd135d699317c2b87f08d00f170339662921dd3d
Ali.es <ali.esmailnejad96@gmail.com>
2022-02-17 01:03:12 +0330

    implement TDD for Discount Model

---
+++ product/tests/test_model_discount.py
@@ -0,0 +1,44 @@
+import datetime
+from django.test import TestCase
+from ..models import Discount
+
+
+class DiscountTest(TestCase):
+
+    def setUp(self) -> None:
+        tomorrow = datetime.timedelta(days=1) + datetime.datetime.now()
+        yesterday = datetime.timedelta(days=-1) + datetime.datetime.now()
+
+        self.discount1 = Discount.objects.create(name='discount1', type='PR', amount=5000,
+                                                 expire_time=tomorrow)
+        self.discount2 = Discount.objects.create(name='discount2', type='PR', amount=10000,
+                                                 expire_time=yesterday)
+
+    def test_discount_not_expired(self):
+        self.assertFalse(self.discount1.is_expire())
+        self.assertTrue(self.discount2.is_expire())
+
+    def test_discount_expired(self):
+        self.assertTrue(self.discount2.is_expire())
+        self.assertFalse(self.discount1.is_expire())
+
+    def test_get_type(self):
+        self.assertEqual(self.discount1.get_type, 'price')
+        self.assertEqual(self.discount2.get_type, 'price')
+
+    def test_profit_amount_expire_discount(self):
+        self.assertEqual(self.discount2.profit_amount(20000), None)
+
+    def test_profit_amount_gt_discount(self):
+        self.assertEqual(self.discount1.profit_amount(20000), 5000)
+        self.assertEqual(self.discount1.profit_amount(30000000), 5000)
+        self.assertEqual(self.discount1.profit_amount(30000000), 5000)
+
+    def test_profit_amount_lt_discount(self):
+        self.assertEqual(self.discount1.profit_amount(2000), 2000)
+        self.assertEqual(self.discount1.profit_amount(200), 200)
+        self.assertEqual(self.discount1.profit_amount(3), 3)
+
+    def test_discount_str(self):
+        self.assertEqual(str(self.discount1), 'discount1')
+        self.assertEqual(str(self.discount2), 'discount2')
