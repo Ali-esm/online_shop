5b8961c9d1af5373443494610c19f2f48e839a37
Ali.es <ali.esmailnejad96@gmail.com>
2022-02-18 20:26:01 +0330

    implement: CustomerUserAdmin to show phone_number on admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,33 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 from .models import User
 
 
-admin.site.register(User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ('phone_number', 'email', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    fieldsets = (
+        (None, {'fields': ('phone_number', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone_number', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('phone_number', )
+    ordering = ('phone_number', )
+
+
+admin.site.register(User, CustomUserAdmin)
