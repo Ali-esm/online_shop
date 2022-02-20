1da15d96abbe5127c4897485ba724b8e685eaf0d
Ali.es <ali.esmailnejad96@gmail.com>
2022-02-21 01:26:52 +0330

    add: logout path to customer app urls

--- customer/urls.py
+++ customer/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from.views import CustomerLoginView, CustomerSignUpView
+from.views import CustomerLoginView, CustomerSignUpView, CustomerLogoutView
 
 app_name = 'customer'
 urlpatterns = [
     path('login/', CustomerLoginView.as_view(), name='login_view'),
     path('signup/', CustomerSignUpView.as_view(), name='signup_view'),
-    # path('logout/', CustomerLogOutView.as_view(), name='logout_view'),
+    path('logout/', CustomerLogoutView.as_view(), name='logout_view'),
 ]
