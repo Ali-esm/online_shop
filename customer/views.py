0ce4ae8563f4f1f730314075759ff344974fe57d
Ali.es <ali.esmailnejad96@gmail.com>
2022-02-21 00:37:07 +0330

    implement: CustomerSignUPView

--- customer/views.py
+++ customer/views.py
@@ -1,8 +1,26 @@
-from django.contrib.auth import views
+from django.contrib.auth import views, login
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy, reverse
 
+from core.models import User
+from .models import Customer
 from .forms import LoginForm, SignUpForm
+
+
+class CustomerSignUpView(views.FormView):
+    template_name = 'customer/signup.html'
+    form_class = SignUpForm
+    success_url = reverse_lazy('home_view')
+
+    def form_valid(self, form):
+        user = User.objects.create_user(password=form.cleaned_data['password2'],
+                                        phone_number=form.cleaned_data['phone_number'])
+        user.first_name = form.cleaned_data['first_name']
+        user.last_name = form.cleaned_data['last_name']
+        user.save()
+        customer = Customer.objects.create(user=user)
+        login(self.request, user)
+        return redirect(reverse('home_view'))
 
 
 class CustomerLoginView(views.LoginView):
