6818c550dbd2485ff8cba4dc64822fa0965e8d31
Ali.es <ali.esmailnejad96@gmail.com>
2022-02-20 23:31:59 +0330

    override: save method in CustomUserCreationForm

--- core/forms.py
+++ core/forms.py
@@ -1,14 +1,32 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.utils.translation import gettext_lazy as _
+
 from .models import User
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('phone_number', )
+        fields = ('username', )
+        labels = {
+            'username': _("Phone Number"),
+        }
+        help_texts = {
+            'username': _("Enter your Phone number")
+        }
+
+    def save(self, commit=True):
+        user = super(CustomUserCreationForm, self).save(commit=False)
+        user.username = user.phone_number
+        if commit:
+            user.save()
+        return user
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = User
-        fields = ('phone_number', )
+        fields = ('username', )
+        labels = {
+            'username': _("Phone Number"),
+        }
