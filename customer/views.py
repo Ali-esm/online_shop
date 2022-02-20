3748c7348b965942e5e789cae351ef320556de74
Ali.es <ali.esmailnejad96@gmail.com>
2022-02-20 23:49:32 +0330

    implement: CustomerLoginView

--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth import views
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
 
-# Create your views here.
+from .forms import LoginForm, SignUpForm
+
+
+class CustomerLoginView(views.LoginView):
+    """
+    Customization LoginView for Login Customers
+    """
+    template_name = 'customer/login.html'
+    authentication_form = LoginForm
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(reverse('home_view'))
+        return super().get(request, *args, **kwargs)
+
+
