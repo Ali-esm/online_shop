3f44751c67397e61f78cbf4edbc67f4f242cfbb5
Ali.es <ali.esmailnejad96@gmail.com>
2022-02-26 01:58:44 +0330

    feat: implement ProductListView & HotProductView

--- product/views.py
+++ product/views.py
@@ -1,3 +1,17 @@
+from django.views import generic
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'product/product-list.html'
+    context_object_name = 'products'
+    paginate_by = 15
+
+
+class HotProductView(generic.ListView):
+    model = Product
+    template_name = 'product/landing.html'
+    context_object_name = 'products'
+    queryset = Product.objects.all()[:3]
