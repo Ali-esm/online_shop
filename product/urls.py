0564cd3d282478325ac9eeed66fdb5ac3921d51b
Ali.es <ali.esmailnejad96@gmail.com>
2022-02-28 01:36:04 +0330

    feat: add product-detail to urls path

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProductListView, HotProductView
+from .views import ProductListView, HotProductView, ProductDetailView
 
 app_name = 'product'
 urlpatterns = [
     path('list/', ProductListView.as_view(), name='product_list'),
+    path('detail/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
 ]
