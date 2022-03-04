2060a83d6d43ca871bcae19e1ae779b17d16e936
Ali.es <ali.esmailnejad96@gmail.com>
2022-03-04 19:44:06 +0330

    feat: implement custom get to filter categories

--- product/views.py
+++ product/views.py
@@ -9,6 +9,15 @@ class ProductListView(generic.ListView):
     context_object_name = 'products'
     paginate_by = 15
 
+    def get(self, request, *args, **kwargs):
+        category = request.GET.get('category', None)
+        if category:
+            context = {
+                'products': Product.objects.filter(category__name=category)
+            }
+            return render(request, 'product/product-list.html', context=context)
+        return super().get(request, *args, **kwargs)
+
 
 class ProductDetailView(generic.DetailView):
     model = Product
