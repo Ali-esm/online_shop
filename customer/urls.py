6f2c92e4ac2f884ae0e48e310a0e93bf1371ce45
Ali.es <ali.esmailnejad96@gmail.com>
2022-03-05 23:11:42 +0330

    feat: add profile path to customer urls

--- customer/urls.py
+++ customer/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from.views import CustomerLoginView, CustomerSignUpView, CustomerLogoutView
+from django.views.generic import TemplateView
+from.views import CustomerLoginView, CustomerSignUpView, CustomerLogoutView,\
+    CustomerProfileView
 
 app_name = 'customer'
 urlpatterns = [
     path('login/', CustomerLoginView.as_view(), name='login_view'),
     path('signup/', CustomerSignUpView.as_view(), name='signup_view'),
     path('logout/', CustomerLogoutView.as_view(), name='logout_view'),
+    path('profile/', CustomerProfileView.as_view(), name='profile_view'),
 ]
