473d7eaa339f6310588560ee5a84caad81a6705b
Ali.es <ali.esmailnejad96@gmail.com>
2022-03-07 01:16:12 +0330

    feat: add CustomerAddressView to customer urls

--- customer/urls.py
+++ customer/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 from.views import CustomerLoginView, CustomerSignUpView, CustomerLogoutView,\
-    CustomerProfileView
+    CustomerProfileView, CustomerAddressView
 
 app_name = 'customer'
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('signup/', CustomerSignUpView.as_view(), name='signup_view'),
     path('logout/', CustomerLogoutView.as_view(), name='logout_view'),
     path('profile/', CustomerProfileView.as_view(), name='profile_view'),
+    path('address/', CustomerAddressView.as_view(), name='address_view'),
 ]
