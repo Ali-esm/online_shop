2bee9f9b1399c2ec1516316b249bdb69bc033d3c
Ali.es <ali.esmailnejad96@gmail.com>
2022-03-21 20:43:01 +0330

    feat: implement view to set cookie

--- order/views.py
+++ order/views.py
@@ -1,3 +1,17 @@
+import json
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class AddProductCookieView(View):
+
+    def post(self, request):
+        # if user authenticated save to session else save to cookie
+        post_body = json.loads(request.body.decode('utf-8'))
+        product_id = post_body.get('product', None)
+        res = HttpResponse('')
+        if product_id:
+            quantity = request.POST.get('quantity', 1)
+            res.set_cookie(f'p{product_id}', str(quantity))
+        return res
