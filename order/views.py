b8f08b4b05bb2ed07e7ab94ae83ca8c0f089f2ab
Ali.es <ali.esmailnejad96@gmail.com>
2022-04-01 14:53:02 +0430

    feat: implement DeleteProductFromCookieView

--- order/views.py
+++ order/views.py
@@ -15,3 +15,15 @@ class AddProductCookieView(View):
             quantity = request.POST.get('quantity', 1)
             res.set_cookie(f'p{product_id}', str(quantity))
         return res
+
+
+class DeleteProductFromCookieView(View):
+
+    def delete(self, request):
+        request_body = json.loads(request.body.decode('utf-8'))
+        product_id = request_body.get('product', None)
+        res = HttpResponse('')
+        print(product_id)
+        if product_id:
+            res.delete_cookie(f'p{product_id}')
+        return res
