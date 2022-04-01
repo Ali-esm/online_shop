cdb77b594461cda8f2d56cc4205d7031b24f693b
Ali.es <ali.esmailnejad96@gmail.com>
2022-04-01 14:52:28 +0430

    feat: add delete-product path to order app urls

--- order/urls.py
+++ order/urls.py
@@ -3,6 +3,7 @@ from . import apis, views
 
 app_name = 'order'
 urlpatterns = [
-    path('add-to-cart/', views.AddProductCookieView.as_view(), name='add_to_cart_view'),
+    path('delete-product/', views.DeleteProductFromCookieView.as_view(), name='delete_product'),
+    path('add-product/', views.AddProductCookieView.as_view(), name='add_product'),
     path('get-products/', apis.GetProductCookieAPIView.as_view(), name='get_products'),
 ]
