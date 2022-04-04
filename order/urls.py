ca38bf14a9649290ecc2c263ada214bad0301498
Ali.es <ali.esmailnejad96@gmail.com>
2022-04-04 21:02:55 +0430

    feat: add items path to order url

--- order/urls.py
+++ order/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('delete-product/', views.DeleteProductFromCookieView.as_view(), name='delete_product'),
     path('add-product/', views.AddProductCookieView.as_view(), name='add_product'),
     path('get-products/', apis.GetProductCookieAPIView.as_view(), name='get_products'),
+    path('items/', views.OrderItemsView.as_view(), name='items_view'),
 ]
