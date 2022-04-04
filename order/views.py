dbd231bc74cc7d9c3642b7aee70aa2ff91033969
Ali.es <ali.esmailnejad96@gmail.com>
2022-04-04 21:03:53 +0430

    refactor: replace session with cookie

--- order/views.py
+++ order/views.py
@@ -1,7 +1,12 @@
 import json
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import View
+
+from .models import Order, OrderItem
+from customer.models import Customer
 
 
 class AddProductCookieView(View):
@@ -12,10 +17,7 @@ class AddProductCookieView(View):
         quantity = request.POST.get('quantity', 1)  # this line maybe have problem
         res = HttpResponse('')
         if product_id:
-            if request.user.is_authenticated:
-                request.session[f'p{product_id}'] = str(quantity)
-            else:
-                res.set_cookie(f'p{product_id}', str(quantity))
+            request.session[f'p{product_id}'] = str(quantity)
             return res
 
 
@@ -26,8 +28,17 @@ class DeleteProductFromCookieView(View):
         product_id = request_body.get('product', None)
         res = HttpResponse('')
         if product_id:
-            if request.user.is_authenticated:
-                del request.session[f'p{product_id}']
-            else:
-                res.delete_cookie(f'p{product_id}')
+            del request.session[f'p{product_id}']
         return res
+
+
+class OrderItemsView(LoginRequiredMixin, View):
+
+    def get(self, request):
+        customer = Customer.objects.get(user__phone_number=request.user.phone_number)
+        if customer.address_set.count() == 0:
+            return redirect(reverse('customer:address_view'))
+        products = request.session.items()
+        order = Order.get_or_create_order()
+        print(products)
+        return render(request, 'order/order_items.html')
