290844a1e94ce7b8d5e453b68307c4f3a8f937c1
Ali.es <ali.esmailnejad96@gmail.com>
2022-04-28 00:48:43 +0430

    feat: add OrderOffCodeAPIView to urls

--- order/urls.py
+++ order/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('remove-item/', views.RemoveOrderItem.as_view(), name='remove_item'),
     path('change-status/', views.ChangeOrderStatus.as_view(), name='change_status'),
     path('change-count/', apis.OrderItemCountPartialUpdate.as_view(), name='change_count'),
+    path('off-code/', apis.OrderOffCodeAPIView.as_view(), name='submit_offcode'),
 ]
