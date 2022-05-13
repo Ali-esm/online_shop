0d39acd8848340f05403b6f031af2b29c337aba3
Ali.es <ali.esmailnejad96@gmail.com>
2022-05-13 19:41:12 +0430

    [STYLE]: reformat code

--- customer/forms.py
+++ customer/forms.py
@@ -8,107 +8,133 @@ from .models import Address, Customer
 
 
 class SignUpForm(CustomUserCreationForm):
-    password1 = forms.CharField(label='',
-                                widget=forms.PasswordInput(attrs={
-                                    'class': 'col-10 p-2',
-                                    'placeholder': _('Password'),
-                                }))
-    password2 = forms.CharField(label='',
-                                widget=forms.PasswordInput(attrs={
-                                    'class': 'col-10 p-2',
-                                    'placeholder': _('Confirm Password')
-                                }))
+    password1 = forms.CharField(
+        label="",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "col-10 p-2",
+                "placeholder": _("Password"),
+            }
+        ),
+    )
+    password2 = forms.CharField(
+        label="",
+        widget=forms.PasswordInput(
+            attrs={"class": "col-10 p-2", "placeholder": _("Confirm Password")}
+        ),
+    )
 
     class Meta:
         model = User
-        fields = ['phone_number', 'first_name', 'last_name', 'password1', 'password2']
+        fields = ["phone_number", "first_name", "last_name", "password1", "password2"]
         labels = {
-            'phone_number': '',
-            'first_name': '',
-            'last_name': '',
+            "phone_number": "",
+            "first_name": "",
+            "last_name": "",
         }
 
         widgets = {
-            'phone_number': forms.TextInput(attrs={
-                'class': 'col-10 p-2',
-                'placeholder': _('Phone'),
-            }),
-            'first_name': forms.TextInput(attrs={
-                'class': 'col-10 p-2',
-                'placeholder': _('First Name'),
-            }),
-            'last_name': forms.TextInput(attrs={
-                'class': 'col-10 p-2',
-                'placeholder': _('Last Name'),
-            }),
+            "phone_number": forms.TextInput(
+                attrs={
+                    "class": "col-10 p-2",
+                    "placeholder": _("Phone"),
+                }
+            ),
+            "first_name": forms.TextInput(
+                attrs={
+                    "class": "col-10 p-2",
+                    "placeholder": _("First Name"),
+                }
+            ),
+            "last_name": forms.TextInput(
+                attrs={
+                    "class": "col-10 p-2",
+                    "placeholder": _("Last Name"),
+                }
+            ),
         }
 
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'col-10 p-2',
-        'placeholder': _('Phone number'),
-    }))
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "col-10 p-2",
+                "placeholder": _("Phone number"),
+            }
+        )
+    )
 
-    password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'col-10 p-2',
-        'placeholder': _('Password'),
-    }))
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "col-10 p-2",
+                "placeholder": _("Password"),
+            }
+        )
+    )
 
     class Meta:
         model = User
-        fields = ['username', 'password']
+        fields = ["username", "password"]
 
-        error_messages = {
-            'login_error': _("Phone Number or Password is Incorrect")
-        }
+        error_messages = {"login_error": _("Phone Number or Password is Incorrect")}
 
         labels = {
-            'username': '',
-            'password': '',
+            "username": "",
+            "password": "",
         }
         widgets = {
-            'username': forms.TextInput(attrs={
-                'class': 'col-10 p-2',
-                'placeholder': _('Phone'),
-            }),
-            'password': forms.PasswordInput(attrs={
-                'class': 'col-10 p-2',
-                'placeholder': _('Password')
-            })
+            "username": forms.TextInput(
+                attrs={
+                    "class": "col-10 p-2",
+                    "placeholder": _("Phone"),
+                }
+            ),
+            "password": forms.PasswordInput(
+                attrs={"class": "col-10 p-2", "placeholder": _("Password")}
+            ),
         }
 
 
 class AddressForm(forms.ModelForm):
     class Meta:
         model = Address
-        fields = ['city', 'province', 'street', 'alley', 'No', 'zip_code']
+        fields = ["city", "province", "street", "alley", "No", "zip_code"]
         widgets = {
-            'city': forms.TextInput(attrs={
-                'class': 'form-control',
-            }),
-            'province': forms.TextInput(attrs={
-                'class': 'form-control',
-            }),
-            'street': forms.TextInput(attrs={
-                'class': 'form-control'
-            }),
-            'alley': forms.TextInput(attrs={
-                'class': 'form-control',
-            }),
-            'No': forms.NumberInput(attrs={
-                'class': 'form-control',
-            }),
-            'zip_code': forms.NumberInput(attrs={
-                'class': 'form-control',
-            }),
+            "city": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                }
+            ),
+            "province": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                }
+            ),
+            "street": forms.TextInput(attrs={"class": "form-control"}),
+            "alley": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                }
+            ),
+            "No": forms.NumberInput(
+                attrs={
+                    "class": "form-control",
+                }
+            ),
+            "zip_code": forms.NumberInput(
+                attrs={
+                    "class": "form-control",
+                }
+            ),
         }
 
     def clean_zip_code(self):
         """
         This method used for check zip code length equals to 10
         """
-        zip_code = self.cleaned_data['zip_code']
+        zip_code = self.cleaned_data["zip_code"]
         if len(zip_code) != 10:
             raise ValidationError(_("Zip code length must be 10"))
         return zip_code
@@ -116,20 +142,19 @@ class AddressForm(forms.ModelForm):
 
 class UpdateUserProfileForm(forms.ModelForm):
     GENDER_CHOICES = (
-        ('M', 'male'),
-        ('F', 'female'),
-        ('N', 'None'),
+        ("M", "male"),
+        ("F", "female"),
+        ("N", "None"),
     )
     gender = forms.ChoiceField(choices=GENDER_CHOICES, initial=GENDER_CHOICES[2][0])
-    birth_date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
-    phone = forms.CharField(disabled=True, label='Phone Number')
+    birth_date = forms.DateField(widget=forms.TextInput(attrs={"type": "date"}))
+    phone = forms.CharField(disabled=True, label="Phone Number")
 
     class Meta:
         model = User
-        fields = ['phone', 'last_name', 'first_name', 'birth_date', 'gender']
+        fields = ["phone", "last_name", "first_name", "birth_date", "gender"]
 
 
 class ContactForm(forms.Form):
     email = forms.EmailField(max_length=200)
     message = forms.CharField(widget=forms.Textarea, max_length=1000)
-
